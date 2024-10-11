bff4116c3ac670c430f4d25bc18ea74d0e6ff08c
t3nta <10407524@ucn.dk>
2024-10-11 17:10:57 +0200

    done

--- AngularPortfolioWebsite/src/app/resume/resume.component.ts
+++ AngularPortfolioWebsite/src/app/resume/resume.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { AccordionModule} from 'ngx-bootstrap/accordion'
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [],
+  imports: [AccordionModule, NgIf],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
 
-  constructor(private titleService: Title) {
+  isWorkExperienceOpen: boolean = false;
+  
+  constructor(private titleService: Title, private renderer: Renderer2) {
     this.titleService.setTitle("Pavlo Skyba - Resume");
   }  
+
+  DownloadFile() {
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_black');
+    link.setAttribute('href', '/AnsøgningPS.pdf');
+    link.setAttribute('download', 'AnsøgningPS');
+    link.click();
+    link.remove();
+  }
 }
